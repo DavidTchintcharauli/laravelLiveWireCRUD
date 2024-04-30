0ef1f89c3db5cacf390953cca1f49b85df0636f8
DavidTchintcharauli <chincharaulidavid@gmail.com>
2024-04-30 23:26:43 +0400

    create delete action listener inside Post.php

--- app/Livewire/Post.php
+++ app/Livewire/Post.php
@@ -6,6 +6,12 @@ use Livewire\Component;
 
 class Post extends Component
 {
+    public $posts, $title, $description, $postId, $updatePost = false, $addPost = false;
+
+    protected $listeners = [
+        'deletePostListner'=>'deletePost'
+    ];
+
     public function render()
     {
         return view('livewire.post');
